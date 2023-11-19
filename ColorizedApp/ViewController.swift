1a55a960eb1fdb8d7fddbcd40093c11a2dd9d94b
Zininas <zininas@mail.ru>
2023-11-19 21:20:56 +0300

    Final version

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var multicoloredView: UIView!
+    
+    @IBOutlet weak var redSliderLabel: UILabel!
+    @IBOutlet weak var greenSliderLabel: UILabel!
+    @IBOutlet weak var blueSliderLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        multicoloredView.layer.cornerRadius = 15
+        redSliderLabel.text = (round((redSlider.value) * 100 ) / 100).formatted()
+        greenSliderLabel.text = (round((greenSlider.value) * 100 ) / 100).formatted()
+        blueSliderLabel.text = (round((blueSlider.value) * 100 ) / 100).formatted()
     }
-
-
+    @IBAction func redSliderAction() {
+        redSliderLabel.text = (round((redSlider.value) * 100 ) / 100).formatted()
+        multicoloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func greenSliderAction() {
+        greenSliderLabel.text = (round((greenSlider.value) * 100 ) / 100).formatted()
+        multicoloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func blueSliderAction() {
+        blueSliderLabel.text = (round((blueSlider.value) * 100 ) / 100).formatted()
+        multicoloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
